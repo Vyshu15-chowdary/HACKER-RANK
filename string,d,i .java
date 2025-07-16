cf32f33866351d3801068cdecdf8b411f3e8cc25
vyshnavi <vishuchowdary20@gmail.com>
2025-07-16 22:38:20 +0530

    string ,int,double

---
+++ string,d,i .java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+       Scanner scanner = new Scanner(System.in);
+     
+      int i = scanner.nextInt();
+      double d = scanner.nextDouble();
+      
+      scanner.nextLine();
+    
+    String s = scanner.nextLine();
+    
+    
+    
+    
+     System.out.println ("String: "  + s);
+    System.out.println("Double: "+"" +d);
+    System.out.println("Int: "+""+i);
+     
+   
+    
+    scanner.close(); 
+        
+        
+    }
+    
+   
+}
